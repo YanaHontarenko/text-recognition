97270b97027a1b26a8b8c00340783815874f0183
Yana Hontarenko <yana.hontarenko@gmail.com>
2020-12-12 16:21:28 +0200

    Recognizer resource backbone

--- resources/text_recognizer.py
+++ resources/text_recognizer.py
@@ -1,3 +1,4 @@
+from flask import render_template
 from flask_restful import Resource, reqparse
 from werkzeug.datastructures import FileStorage
 
@@ -7,4 +8,10 @@ class Recognizer(Resource):
     _parser.add_argument('image', type=FileStorage, required=True, help="This field cannot be blank.", location='files')
 
     def post(self, model_type):
-        return "Returned result"
+        data = self._parser.parse_args()
+        image = data['image'].read()
+        print(image)
+        return model_type
+
+    def get(self):
+        return render_template("recognized.html")
