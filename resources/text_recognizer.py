5c8788f88507b50ca58bbd0ca490e2137c37209b
Yana Hontarenko <yana.hontarenko@gmail.com>
2020-12-13 20:48:11 +0200

    Add temporary saving in recognizer

--- resources/text_recognizer.py
+++ resources/text_recognizer.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import render_template, url_for, redirect, make_response
 from flask_restful import Resource, reqparse
 from werkzeug.datastructures import FileStorage
@@ -10,11 +12,12 @@ class Recognizer(Resource):
     def post(self, model_type):
         data = self._parser.parse_args()
         image = data['image'].read()
+        with open(os.path.join("static", "tempImg.jpg"), "wb") as file:
+            file.write(image)
         return self.redirect(model_type)
 
     def redirect(self, model_type):
         return "/recognize/" + model_type
 
     def get(self, model_type):
-        print(model_type)
-        return make_response(render_template("recognized.html", model_type=model_type))
+        return make_response(render_template("recognized.html", message="Used time", image_path="tempImg.jpg"))
