c2f5c76779e4446a90dfc4fd454bf788ccefd6d0
Yana Hontarenko <y.hontarenko@spd-ukraine.com>
2020-12-14 19:37:11 +0200

    Fix paths

--- custom/plot_sar_accuracy.py
+++ custom/plot_sar_accuracy.py
@@ -5,11 +5,11 @@ import numpy as np
 
 from dataset.dataset import iiit5k_mat_extractor
 
-with open(os.path.join("sar-pytorch", "str", "predict.txt"), "r") as file:
+with open(os.path.join("data", "sar-results", "predict.txt"), "r") as file:
     data = file.read()
     data = np.array([line.split(" ") for line in data.split("\n")])
 
-annotation = iiit5k_mat_extractor("../../data/IIIT5K/testdata.mat")
+annotation = iiit5k_mat_extractor(os.path.join("data", "IIIT5K", "testdata.mat"))
 ann_dict = {}
 for ann in annotation:
     ann_dict[ann[0][5:]] = ann[1]
