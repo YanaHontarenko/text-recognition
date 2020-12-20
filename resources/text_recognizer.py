0ed2f700dfda00eaa17646f40bb90ca88efa08c3
Yana Hontarenko <y.hontarenko@spd-ukraine.com>
2020-12-20 15:59:20 +0200

    Fix text recognizer resource

--- resources/text_recognizer.py
+++ resources/text_recognizer.py
@@ -1,8 +1,17 @@
+import sys
 import os
+sys.path.append(os.path.join(os.getcwd(), "CRAFT-pytorch"))
+sys.path.append(os.path.join(os.getcwd(), "CRNN_Tensorflow"))
+sys.path.append(os.path.join(os.getcwd(), "MaskTextSpotter"))
+sys.path.append(os.path.join(os.getcwd(), "sar-pytorch"))
+sys.path.append(os.path.join(os.getcwd()))
+
+import numpy as np
+import cv2
 
 from flask import render_template, make_response
 from flask_restful import Resource, reqparse
-from models.text_recognizer import TextRecognizerModel
+from model.text_recognizer import TextRecognizerModel
 from werkzeug.datastructures import FileStorage
 
 
@@ -13,7 +22,8 @@ class Recognizer(Resource):
     def post(self, model_type):
         data = self._parser.parse_args()
         image = data['image'].read()
-        common_time = 0
+        nparr = np.fromstring(image, np.uint8)
+        image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
         if model_type == "craft-crnn":
             image, common_time = TextRecognizerModel.craft_crnn(image)
         elif model_type == "craft-sar":
@@ -23,10 +33,8 @@ class Recognizer(Resource):
         elif model_type == "east-sar":
             image, common_time = TextRecognizerModel.east_sar(image)
         else:
-            pass
-            # TODO: Add masktextspotter model
-        with open(os.path.join("static", "tempImg.jpg"), "wb") as file:
-            file.write(image)
+            image, common_time = TextRecognizerModel.mask(image)
+        cv2.imwrite(os.path.join("static", "tempImg.jpg"), image)
         with open(os.path.join("static", "time.txt"), "w") as file:
             file.write(str(common_time))
         return self.redirect(model_type)
@@ -35,7 +43,7 @@ class Recognizer(Resource):
         return "/recognize/" + model_type
 
     def get(self, model_type):
-        with open(os.path.join("static", "time.txt"), "w") as file:
+        with open(os.path.join("static", "time.txt"), "r") as file:
             common_time = float(file.read())
         result_string = TextRecognizerModel.create_result_string(model_type, common_time)
         return make_response(render_template("recognized.html", message=result_string, image_path="tempImg.jpg"))
